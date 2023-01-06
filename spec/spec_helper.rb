63360ecdf329fd89d5df523085e4a195718c7a25
ml-kristoffer-bergman <kristoffer.bergman@monstar-lab.com>
2023-01-06 17:24:15 +0900

    Change JSON formatter for simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-require 'simplecov-json'
+require 'simplecov_json_formatter'
 
 # Generate HTML and JSON reports
 SimpleCov.formatters = SimpleCov::Formatter::MultiFormatter.new([
